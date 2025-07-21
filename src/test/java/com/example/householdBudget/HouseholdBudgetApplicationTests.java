1146b3df61160e1e601aa9776e23b9a357fbe68c
Wiewior <wasisnkipatryk@gmail.com>
2025-07-21 15:12:28 +0200

    first commit

---
+++ src/test/java/com/example/householdBudget/HouseholdBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.householdBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
